7d1e395bf78915fe6e26a5c35bdf1e7cae2ceba7
lipatoS <81018476+lipatoS@users.noreply.github.com>
2021-11-15 16:59:38 +0200

    Delete app directory

--- app/settings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from currency.views import hello_world
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello_world)
-]
